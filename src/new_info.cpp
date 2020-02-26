c3f2ca3f6d38d78adfb953352e0bba770687c6c9
MiSeon23 <maisie0329@gmail.com>
2020-02-26 12:13:23 +0900

    add new_scan

---
+++ src/new_info.cpp
@@ -0,0 +1,18 @@
+#include "ros/ros.h"
+#include "rplidar_ros/new_rplidar.h"
+
+void msgCallback(const rplidar_ros::new_rplidar::ConstPtr& msg){
+    int count = msg->count;
+    for(int i=0; i<count; i++)
+        ROS_INFO("new info : [%f, %f]", msg->degree[i], msg->distance[i]);
+}
+
+int main(int argc, char **argv){
+    ros::init(argc, argv, "new_info");
+    ros::NodeHandle n;
+    ros::Subscriber sub = n.subscribe("new_scan", 1000, msgCallback);
+
+    ros::spin();
+
+    return 0;
+}
